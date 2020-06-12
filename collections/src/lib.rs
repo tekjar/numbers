738f473b2fa5072c671857b6c3166015f001cdb2
tekjar <raviteja@bytebeam.io>
2020-06-12 15:05:16 +0530

    Add access functions

--- collections/src/lib.rs
+++ collections/src/lib.rs
@@ -42,22 +42,12 @@ fn access_vec(v: &Vec<Option<u64>>, indices: &Vec<usize>) -> u64 {
     out
 }
 
-fn access_map(v: &HashMap<String, u64>, indices: &Vec<String>) -> u64 {
-    let mut out = 0;
-    for i in indices {
-        out += *v.get(i).unwrap();
-    }
-
-    out
+fn access_map(v: &HashMap<String, u64>, i: &str) -> u64 {
+    *v.get(i).unwrap()
 }
 
-fn access_seahash_map(v: &HashMap<String, u64, RandomState<Hash64>>, indices: &Vec<String>) -> u64 {
-    let mut out = 0;
-    for i in indices {
-        out += *v.get(i).unwrap();
-    }
-
-    out
+fn access_seahash_map(v: &HashMap<String, u64, RandomState<Hash64>>, i: &str) -> u64 {
+    *v.get(i).unwrap()
 }
 
 #[cfg(test)]
@@ -106,38 +96,30 @@ mod tests {
     }
 
     #[bench]
-    fn access_vec_1_000_000(b: &mut Bencher) {
-        let indices: Vec<usize> = (0..1_000_000).map(|_| rand::thread_rng().gen_range(0, 1_000_000)).collect();
+    fn access_vec_10(b: &mut Bencher) {
+        let indices: Vec<usize> = (0..10).map(|_| rand::thread_rng().gen_range(0, 1_000_000)).collect();
         let v = vec![Some(1); 1_000_000];
         b.iter(|| {
             access_vec(&v, &indices)
         });
-
-        b.bytes = v.len() as u64 * 8;
     }
 
     #[bench]
-    fn access_map_1_000_000(b: &mut Bencher) {
+    fn access_map_1(b: &mut Bencher) {
         let mut map = HashMap::new();
-        for i in 0..100_000 {
+        for i in 0..1_000_000 {
             let topic = "hello/world".to_owned() + &i.to_string();
             map.insert(topic, 1u64);
         }
 
-        let indices: Vec<String> = (0..1_00_000).map(|_| {
-            let i = rand::thread_rng().gen_range(0, 1_000_000);
-            "hello/world".to_owned() + &i.to_string()
-        }).collect();
-
+        let i = "hello/world".to_owned() + &rand::thread_rng().gen_range(0, 1_000_000).to_string();
         b.iter(|| {
-            access_map(&map, &indices)
+            access_map(&map, &i)
         });
-
-        b.bytes = indices.len() as u64 * 8;
     }
 
     #[bench]
-    fn access_seahash_map_100_000(b: &mut Bencher) {
+    fn access_seahash_map_1(b: &mut Bencher) {
         let s = RandomState::<Hash64>::new();
         let mut map = HashMap::with_hasher(s);
         for i in 0..1_000_000 {
@@ -145,16 +127,10 @@ mod tests {
             map.insert(topic, 1u64);
         }
 
-        let indices: Vec<String> = (0..1_00_000).map(|_| {
-            let i = rand::thread_rng().gen_range(0, 1_000_000);
-            "hello/world".to_owned() + &i.to_string()
-        }).collect();
-
+        let i = "hello/world".to_owned() + &rand::thread_rng().gen_range(0, 1_000_000).to_string();
         b.iter(|| {
-            access_seahash_map(&map, &indices)
+            access_seahash_map(&map, &i)
         });
-
-        b.bytes = indices.len() as u64 * 8;
     }
 }
 
