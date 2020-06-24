5975770ee5dadc54da08267d02909da437c74a05
tekjar <raviteja@bytebeam.io>
2020-06-24 18:37:18 +0530

    add plot utility

--- minimqtt/mergedbytes/src/bin/run.sh
+++ minimqtt/mergedbytes/src/bin/run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-cargo run --release --bin smol | tee smol.txt
-cargo run --release --bin tokio | tee tokio.txt
+cargo run --release --bin smol | tee 1.txt
+cargo run --release --bin tokio | tee 1.txt
 
-
-cargo run --release --bin smol -f 500  | tee smol.txt
-cargo run --release --bin tokio -f 500 | tee tokio.txt
+cargo run --release --bin smol -- -f 500  | tee -a 2.txt
+cargo run --release --bin tokio -- -f 500 | tee -a 2.txt
